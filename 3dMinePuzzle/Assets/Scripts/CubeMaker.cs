483c2f00e6a5e501f0bb99ddfa96df37b4bb0354
Myungjee Jung <myungjee@naver.com>
2019-12-04 22:40:47 +0100

    make cube generated by order, top to down, only certain rows visible

--- 3dMinePuzzle/Assets/Scripts/CubeMaker.cs
+++ 3dMinePuzzle/Assets/Scripts/CubeMaker.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,39 +9,75 @@ namespace cubepuzzle
     {
         [SerializeField]
         private GameObject cubePrefab;
-        private GameObject parentGameObject;
 
-        private List<GameObject> cubeList = new List<GameObject>();
+        private List<GameObject[]> cubeList = new List<GameObject[]>();
 
         [SerializeField]
         private int numberOfCubesPerRow;
+
         [SerializeField]
         private int numberOfColumn;
 
-        // Start is called before the first frame update
-        void Start()
-        {
-            parentGameObject = new GameObject();
+        private float devideByHalf;
+        private float rest;
 
-            for (int i = 0; i < numberOfCubesPerRow; i++)
+        public float Min;
+        
+        void Awake()
+        {
+            devideByHalf = (float)numberOfCubesPerRow / 2f;
+            rest = devideByHalf - (int)devideByHalf;
+            Min = -devideByHalf + rest;
+
+            GenerateCubes();
+        }
+
+        internal Vector3 StartingPosition()
+        {
+            return new Vector3((numberOfCubesPerRow - devideByHalf + rest - 1), numberOfCubesPerRow, (numberOfCubesPerRow - devideByHalf + rest - 1));
+        }
+
+        // generate cubes as a map
+        private void GenerateCubes()
+        {
+            GameObject parentGameObject = new GameObject();
+            parentGameObject.name = "Cube Holder";
+
+            // generate each column
+            for (int k = 0; k < numberOfColumn; k++)
             {
-                for (int j = 0; j < numberOfCubesPerRow; j++)
+                GameObject columnParent = new GameObject();
+                columnParent.transform.position = Vector3.zero;
+                columnParent.name = "Column" + k.ToString();
+                columnParent.transform.parent = parentGameObject.transform;
+
+                columnParent.SetActive(k < numberOfCubesPerRow);
+
+                GameObject[] oneColumn = new GameObject[numberOfCubesPerRow * numberOfCubesPerRow];
+
+                // generate each cube
+                for (int i = 0; i < numberOfCubesPerRow; i++)
                 {
-                    for (int k = 0; k < numberOfColumn; k++)
+                    for (int j = 0; j < numberOfCubesPerRow; j++)
                     {
+                        columnParent.transform.position = new Vector3(0, -k + numberOfCubesPerRow - 0.5f, 0);
+
                         GameObject cube = Instantiate(cubePrefab);
-                        cube.transform.position = new Vector3(0.5f * (i - 1), 0.5f * k, 0.5f * (j - 1));
-                        cube.transform.parent = parentGameObject.transform;
-                        cubeList.Add(cube);
+                        cube.transform.parent = columnParent.transform;
+                        cube.transform.localPosition = new Vector3(i - devideByHalf + rest, 0, j - devideByHalf + rest);
+
+                        Debug.Log("i : " + i + " truncate : " + devideByHalf + " rest : " + rest + " hm : " + (i - devideByHalf + rest));
+                        oneColumn[i * 3 + j] = cube;
+
+                        cubeList.Add(oneColumn);
                     }
                 }
             }
         }
 
-        // Update is called once per frame
-        void Update()
+        internal void MoveDown()
         {
-
+            
         }
     }
 }
