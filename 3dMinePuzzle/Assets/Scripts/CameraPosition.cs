ffd912165220061de123f64bf50bcb8eb41d564e
Myungjee Jung <m.jung2@salt-and-pepper.eu>
2019-12-13 19:05:51 +0100

    fix camera position based on the number of cubes in a row

---
+++ 3dMinePuzzle/Assets/Scripts/CameraPosition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace cubepuzzle
+{
+    public class CameraPosition : MonoBehaviour
+    {
+        [Inject] private ICubeMaker cubeMaker;
+        [SerializeField] private float someValue;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            this.transform.position -= this.transform.forward * (cubeMaker.NumberOfCubesPerRow - 3) * someValue;
+        }
+    }
+}
